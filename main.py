0d23737fd583c47d7cba760ba59dbc94d61a6b19
SenhorHaki <darley.ks3@gmail.com>
2021-05-24 00:44:29 -0300

    teste de audio

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Arquivo principal.
+import speech_recognition as sr
+import pyaduio
+
+#cria um reconhecedor
+r = sr.Recognizer()
+
+#Abrindo o microfone para captura
+with sr.Microphone() as source:
+    audio = r.listen(source)
+
+    print(r.recognize_google(audio))
